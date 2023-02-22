89a11562333871b2c2e22fc91cd0f3611935ab2f
minsol <0206cho@gmail.com>
2023-02-23 04:25:14 +0900

    민솔 - 로그인페이지

--- skimate-front/src/pages/Login.vue
+++ skimate-front/src/pages/Login.vue
@@ -1,53 +1,82 @@
 <template>
-  <div class="form-signin w-100 m-auto">
-    <h1 class="h3 mb-3 fw-normal">로그인</h1>
-
-    <div class="form-floating">
-      <input
-        type="email"
-        class="form-control"
-        id="floatingInput"
-        placeholder="name@example.com"
-        v-model="state.form.memberId"
-      />
-      <label for="floatingInput">사용자 ID</label>
-    </div>
-    <div class="form-floating">
-      <input
-        type="password"
-        class="form-control"
-        id="floatingPassword"
-        placeholder="Password"
-        v-model="state.form.memberPw"
-      />
-      <label for="floatingPassword">비밀번호</label>
-    </div>
-
-    <div>
-      <button class="w-100 btn btn-lg btn-primary" @click="submit()">
+  <v-row>
+    <v-col cols="2"></v-col>
+    <v-col cols="1">
+      <h1 class="pt-12 px-0" style="display: flex; justify-content: center; color:#053D68; font-family:'Title_bold'">|
+      </h1>
+    </v-col>
+    <v-col cols="6">
+      <h1 class="pt-12 px-0" style="display: flex; justify-content: center; color:#053D68; font-family:'Title_bold'">
         로그인
-      </button>
-    </div>
-    <div>
-      <a :href="naverurl">
-        <img src="/img/naverLogin.png" class="sociaLogin">
-      </a>
-    </div>
-    <div>
-      <a :href="kakaourl">
-        <img src="/img/kakaoLogin.png" class="sociaLogin">
-      </a>
-    </div>
-    <div>
-      <a :href="googleUrl">
-        <img src="/img/googleLogin.png" class="sociaLogin">
-      </a>
-    </div>
+      </h1>
+    </v-col>
+    <v-col cols="1">
+      <h1 class="pt-12 px-0" style="display: flex; justify-content: center; color:#053D68; font-family:'Title_bold'">|
+      </h1>
+    </v-col>
+    <v-col cols="2"></v-col>
+  </v-row>
 
-    <div>
-      <button class="w-100 btn btn-lg btn-primary" @click="joinForm()">회원가입</button>
-    </div>
-  </div>
+  <v-row>
+    <v-col cols="2"></v-col>
+    <v-col cols="8" class="center">
+      <v-divider style="border-color:#053D68;"></v-divider><!--=hr-->
+      <v-row>
+        <v-col cols="3"></v-col>
+        <v-col cols="6">
+          <div class="form-signin cetner ml-4" style="min-width: 600px; padding:0px;">
+            <div class="form-floating">
+              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"
+                v-model="state.form.memberId"/>
+              <label for="floatingInput">사용자 ID</label>
+            </div>
+            <div class="form-floating">
+              <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
+                v-model="state.form.memberPw" />
+              <label for="floatingPassword">비밀번호</label>
+            </div>
+            <v-row>
+              <v-col cols="6">
+                <v-btn class="white--text" rounded="pill" style="font-size: large; width:100%; height: 60px; background-color:#053D68; color: white;" @click="submit()">로그인</v-btn>
+              </v-col>
+              <v-col cols="6">
+                <v-btn class="white--text" rounded="pill" style="font-size: large; width:100%; height: 60px; background-color:#053D68; color: white;" @click="joinForm()">회원가입</v-btn>
+              </v-col>
+            </v-row>
+            <br>
+            <v-row>
+              <v-col cols="3"></v-col>
+              <v-col cols="7" class="ml-4">
+                <a :href="naverurl">
+                  <img src="/img/naver.png" class="sociaLogin mr-8" style="width: 20%;">
+                </a>
+                <a :href="kakaourl">
+                  <img src="/img/kakao.png" class="sociaLogin  mr-8" style="width: 20%;">
+                </a>
+                <a :href="googleUrl">
+                  <img src="/img/google.png" class="sociaLogin" style="width: 20%;">
+                </a>
+              </v-col>
+            </v-row>
+            <div>
+              
+            </div>
+            <div>
+              
+            </div>
+            <div>
+              
+            </div>
+        
+          </div>
+        </v-col>
+        <v-col cols="3"></v-col>
+      </v-row>
+      
+    </v-col>
+    <v-col cols="1"></v-col>
+  </v-row>
+  
 </template>
 
 <script>
@@ -65,10 +94,10 @@ export default {
       },
     });
 
-    const joinForm = () =>{
-      router.push({path : "/join"});
+    const joinForm = () => {
+      router.push({ path: "/join" });
     }
-    
+
     const submit = () => {
       axios
         .post("http://localhost:8080/login", state.form)
@@ -84,8 +113,8 @@ export default {
         });
     };
 
-  
-    
+
+
     const naverurl = ref("http://localhost:8080/oauth2/authorization/naver?redirect_url=http://localhost:3000/oauth/redirect");
     const kakaourl = ref("http://localhost:8080/oauth2/authorization/kakao?redirect_url=http://localhost:3000/oauth/redirect");
     const googleUrl = ref("http://localhost:8080/oauth2/authorization/google?redirect_url=http://localhost:3000/oauth/redirect");
@@ -100,14 +129,15 @@ export default {
       joinForm
     };
   },
-  
+
 };
 </script>
 
 <style scoped>
-.sociaLogin{
+.sociaLogin {
   max-width: 250px;
 }
+
 .form-signin {
   max-width: 330px;
   padding: 15px;
