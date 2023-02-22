04676ac7875a65941119b23fc5224a8e01d2b434
minsol <0206cho@gmail.com>
2023-02-23 04:58:47 +0900

    민솔 - lesson 상세페이지 수정중

--- skimate-front/src/pages/lesson/LessonHome.vue
+++ skimate-front/src/pages/lesson/LessonHome.vue
@@ -4,15 +4,16 @@
     <v-row>
       <v-col cols="2"></v-col>
       <v-col cols="1">
-        <h1 class="pt-12 px-0" style="display: flex; justify-content: center; color:#053D68; font-family:'Title_bold'">|
+        <h1 class="pt-12 px-0" style="display: flex; justify-content: center; color:#053D68; font-family:Title_bold;">|
         </h1>
       </v-col>
       <v-col cols="6">
-        <h1 class="pt-12 px-0" style="display: flex; justify-content: center; color:#053D68; font-family:'Title_bold'">강&nbsp;&nbsp;습
+        <h1 class="pt-12 px-0" style="display: flex; justify-content: center; color:#053D68; font-family:'Title_bold';">
+          강&nbsp;&nbsp;습
         </h1>
       </v-col>
       <v-col cols="1">
-        <h1 class="pt-12 px-0" style="display: flex; justify-content: center; color:#053D68; font-family:'Title_bold'">|
+        <h1 class="pt-12 px-0" style="display: flex; justify-content: center; color:#053D68; font-family:'Title_bold';">|
         </h1>
       </v-col>
       <v-col cols="2"></v-col>
@@ -29,49 +30,52 @@
               <v-col cols="9">
                 <v-card flat>
                   <v-card-text>
-                    <v-text-field :loading="loading" density="compact" variant="solo" label="검색"
-                      append-inner-icon="mdi-magnify" single-line hide-details
-                      @click:append-inner="onClick"></v-text-field>
                   </v-card-text>
                 </v-card>
               </v-col>
               <v-col cols="3" class="pt-7">
-                <v-btn class="white--text mr-3 mb-3" style="background-color:#053D68; color: white;" to="/lesson/write">글쓰기</v-btn>
+                <v-btn class="white--text mr-3 mb-3" style="background-color:#053D68; color: white;"
+                  to="/lesson/write">글쓰기</v-btn>
               </v-col>
             </v-row>
           </v-col>
         </v-row>
 
-          <v-table fixed-header height="400px">
-            <thead>
-              <tr>
-                <th class="text-center" style="font-weight: bold; background-color: #BFDDF9;">
-                  순번
-                </th>
-                <th class="text-center" style="font-weight: bold; background-color: #BFDDF9;">
-                  제목
-                </th>
-                <th class="text-center" style="font-weight: bold; background-color: #BFDDF9;">
-                  구분
-                </th>
-                <th class="text-center" style="font-weight: bold; background-color: #BFDDF9;">
-                  강습일
-                </th>
-                <th class="text-center" style="font-weight: bold; background-color: #BFDDF9;">
-                  게시일
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr class="text-center" v-for="(item, idx) in state.lesson" :key="idx">
-                <td>{{ item.lesson_id }}</td>
-                <td>{{ item.lesson_title }}</td>
-                <td>{{ item.lesson_category }}</td>
-                <td>{{ item.lesson_lsdate }}</td>
-                <td>{{ item.lesson_date }}</td>
-              </tr>
-            </tbody>
-          </v-table>
+        <v-table fixed-header height="400px">
+          <thead>
+            <tr>
+              <th class="text-center" style="font-weight: bold; background-color: #BFDDF9;">
+                순번
+              </th>
+              <th class="text-center" style="font-weight: bold; background-color: #BFDDF9;">
+                제목
+              </th>
+              <th class="text-center" style="font-weight: bold; background-color: #BFDDF9;">
+                구분
+              </th>
+              <th class="text-center" style="font-weight: bold; background-color: #BFDDF9;">
+                강습일
+              </th>
+              <th class="text-center" style="font-weight: bold; background-color: #BFDDF9;">
+                게시일
+              </th>
+              <th class="text-center" style="font-weight: bold; background-color: #BFDDF9;">
+                상세보기
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="text-center" v-for="(item, idx) in state.lesson" :key="idx">
+              <td>{{ item.lesson_id }}</td>
+              <td>{{ item.lesson_title }}</td>
+              <td>{{ item.lesson_category }}</td>
+              <td>{{ item.lesson_lsdate }}</td>
+              <td>{{ item.lesson_date }}</td>
+              <td><v-btn class="white--text" style="background-color:#053D68; color: white;"
+                  @click="detail (item.lesson_id)">더보기</v-btn></td>
+            </tr>
+          </tbody>
+        </v-table>
       </v-col>
       <v-col cols="2"></v-col>
     </v-row>
@@ -81,39 +85,69 @@
 <script>
 import axios from "axios";
 import { reactive } from "@vue/reactivity";
+import router from "@/scripts/router";
 export default {
   name: "LessonHome",
-  setup(){
+  setup() {
     const state = reactive({
       lesson: [],
     });
-    axios.get("http://localhost:8080/api/lesson/list",{
-        headers : {
-          'Authorization' : 'Bearer ' + sessionStorage.getItem('id')
-        }
-      }).then((res) => {
-        console.log(res.data);
-        
-        state.lesson = res.data;
-        console.log(state.lesson);
-      });
-    return {state}
-  },
-  data () {
-      return {
+
+    axios.get("http://localhost:8080/api/lesson/list", {
+      headers: {
+        'Authorization': 'Bearer ' + sessionStorage.getItem('id')
       }
-    },
+    }).then((res) => {
+      console.log(res.data);
 
-    methods: {
-      onClick () {
-        this.loading = true
+      state.lesson = res.data;
+      console.log(state.lesson);
+    });
 
-        setTimeout(() => {
-          this.loading = false
-          this.loaded = true
-        }, 2000)
-      },
+    // const remove = (lessonId) => {
+    //   axios.delete(`http://localhost:8080/api/lesson/delete/${lessonId}`).then(() => {
+    //     // load();
+    //   })
+    // }
+
+    const detail = (lessonId) => {
+      axios
+        .post(`http://localhost:8080/api/lesson/detail/${lessonId}`, {
+          headers: {
+            'Authorization': 'Bearer ' + sessionStorage.getItem('id')
+          }
+        })
+        .then((res) => {
+          console.log(">>>>>>>>>>> res : " + res);
+          router.push({ path: `http://localhost:8080/api/lesson/detail/${lessonId}` });
+          window.alert("이동");
+        })
+        .catch(() => {
+          window.alert("다시 보내세요.");
+        });
+    }
+    return { 
+      state, 
+      detail,
+      // remove 
+    }
+  },
+  data() {
+    return {
+
+    }
+  },
+
+  methods: {
+    onClick() {
+      this.loading = true
+
+      setTimeout(() => {
+        this.loading = false
+        this.loaded = true
+      }, 2000)
     },
+  },
 };
 </script>
   
