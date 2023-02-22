b2648f829e66aa9c349d60969283ea2a03c6e968
김종하 <81893082+gbr7780@users.noreply.github.com>
2023-02-23 04:40:19 +0900

    종하 예약 작업중

--- skimate-front/src/main.js
+++ skimate-front/src/main.js
@@ -4,11 +4,15 @@ import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import store from "./scripts/store";
 import router from "./scripts/router";
+import moment from 'moment';
+import VueMoment from "vue-momentjs";
 
 loadFonts()
 
 createApp(App)
   .use(vuetify)
+  .use(moment)
+  .use(VueMoment)
   .use(store)
   .use(router)
   .mount('#app')
