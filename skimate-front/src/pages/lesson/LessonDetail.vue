f4da098e4e6c34153c15f5fa6da9703edba50bdc
김종하 <81893082+gbr7780@users.noreply.github.com>
2023-02-23 14:54:50 +0900

    dasdf

--- skimate-front/src/pages/lesson/LessonDetail.vue
+++ skimate-front/src/pages/lesson/LessonDetail.vue
@@ -62,7 +62,7 @@
 
           <v-card-text class="end text-md-center">
             <v-btn class="white--text mr-3 mb-3" style="background-color:#053D68; color: white;" to="/lesson">목록으로</v-btn>
-            <v-btn class="white--text mr-3 mb-3" style="background-color:#053D68; color: white;" to="/lesson">신청하기</v-btn> <!--결제 api-->
+            <v-btn class="white--text mr-3 mb-3" style="background-color:#053D68; color: white;" @click="PaymentBtn">신청하기</v-btn> <!--결제 api-->
             <!--작성자만 표기-->
             <v-btn class="white--text mr-3 mb-3" style="background-color:#053D68; color: white;" to="/lesson">수정하기</v-btn>
             <v-btn class="white--text mr-3 mb-3" style="background-color:#053D68; color: white;" to="/lesson">삭제하기</v-btn>
@@ -77,9 +77,10 @@
 <script>
 import { useStore } from "vuex";
 import { computed } from "vue";
-//import { useStore } from "vuex";
+import router from "@/scripts/router";
 import axios from "axios";
 import { reactive } from "@vue/reactivity";
+const { IMP } = window;
 export default {
   name: "LessonHome",
   data(){
@@ -100,8 +101,16 @@ setup(){
 
   //console.log("김민솔 : " + storeLessonDetail.value)
 
+  //const store = useStore();
+  const lesson_price = computed(() => state.lesson.lesson_price);
+  const lessonId = computed(() => state.lesson.lessonId);
+
   const state = reactive({
-      lesson: []
+      lesson: [],
+      form: {
+                 price: lesson_price,
+                 lessonId: lessonId,
+              },
     });
 
 
@@ -118,10 +127,24 @@ setup(){
         });
     }
 
+    const submit = () => {
+            axios.post("http://localhost:8080/api/cash/lesson", state.form, {
+                headers: {
+                  Authorization: "Bearer " + sessionStorage.getItem("id"),
+                },
+              })
+              .then((res) => {
+                console.log("res : " + res);
+              })
+              .catch(() => {
+                window.alert("로그인 정보가 처리되지 않았습니다.");
+              });
+              };
+
     return {
       detail,
-      state
-      //storeLessonDetail
+      state,
+      submit
     }
 },
 
@@ -134,6 +157,49 @@ setup(){
         this.loaded = true
       }, 2000)
     },
+
+    PaymentBtn: function () {
+      IMP.init("imp77524147");
+      IMP.request_pay(
+        {
+          // param
+          pg: "html5_inicis",
+          pay_method: "card",
+          merchant_uid: "merchant_" + new Date().getTime(),
+          name: "스키메이트 결제",
+          //amount: this.price,
+          amount: 300,
+          buyer_email: "funidea_woo@naver.com",
+          buyer_name: "테스터",
+          buyer_tel: "010-8832-4280",
+          buyer_addr: "서울특별시 영등포구 당산동",
+          buyer_postcode: "07222",
+        },
+        (rsp) => {
+          // callback
+          console.log(rsp);
+          var msg = "";
+          if (rsp.success) {
+            msg = "결제가 완료되었습니다.\n";
+            msg += "고유ID : " + rsp.imp_uid;
+            msg += "\n상점 거래ID : " + rsp.merchant_uid;
+            msg += "\n결제 금액 : " + rsp.paid_amount;
+            msg += "\n카드 승인번호 : " + rsp.apply_num;
+            console.log("결제 성공");
+            router.push({ path: `/lesson` });
+            this.submit()
+
+          } else {
+            msg = "결제에 실패하였습니다.";
+            msg += "\n실패 내용 : " + rsp.error_msg;
+            console.log("결제 실패");
+          }
+          alert(msg);
+        }
+      );
+    },
+
+
   },
 };
 </script>
